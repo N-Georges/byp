9e75fc7a5c9422e0554119c1a490125f539639a6
nodari georges <ngeorges.dev@gmail.com>
2023-03-05 13:18:51 +0100

    custom client

--- www/src/clients/apollo-client.ts
+++ www/src/clients/apollo-client.ts
@@ -7,8 +7,18 @@ import {
 import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
 import { getMainDefinition } from "@apollo/client/utilities";
 import { createClient } from "graphql-ws";
+import { setContext } from "@apollo/client/link/context";
 
 const isServerSide = typeof window === "undefined";
+
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      "x-hasura-admin-secret": process.env.HASURA_ADMIN_SECRET as string,
+    },
+  };
+});
 
 const httpLink = createHttpLink({
   uri: process.env.NEXT_PUBLIC_HASURA_PROJECT_ENDPOINT,
@@ -48,7 +58,7 @@ const splitLink = wsLink
   : httpLink;
 
 const client = new ApolloClient({
-  link: splitLink,
+  link: authLink.concat(splitLink),
   cache: new InMemoryCache(),
 });
 
